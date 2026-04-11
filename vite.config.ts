db8d5be8e5c65046fecf62edd91475714c2e1499
Alex Knipfer <alexanderknipfer@gmail.com>
2026-04-11 13:40:28 -0400

    initialize project

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vite";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+const config = defineConfig({
+  server: {
+    host: "127.0.0.1",
+  },
+  plugins: [
+    devtools(),
+    tsconfigPaths({ projects: ["./tsconfig.json"] }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ],
+});
+
+export default config;
