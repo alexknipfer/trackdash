cbaf34008631d2b40b652c0ed3be9ab7d5cbbc0f
Alex Knipfer <alexanderknipfer@gmail.com>
2026-04-16 21:57:35 -0400

    add spotify scopes

--- src/lib/auth/auth.server.ts
+++ src/lib/auth/auth.server.ts
@@ -5,6 +5,14 @@ import { env } from '#/env/env.server';
 export const auth = betterAuth({
 	socialProviders: {
 		spotify: {
+			scope: [
+				'user-read-email',
+				'user-top-read',
+				'user-follow-read',
+				'user-read-recently-played',
+				'playlist-read-private',
+				'user-read-currently-playing'
+			],
 			clientId: env.SPOTIFY_CLIENT_ID,
 			clientSecret: env.SPOTIFY_CLIENT_SECRET
 		}
