242ff0a3413f792c0b7842f8b1b5970b552ee618
Alex Knipfer <alexanderknipfer@gmail.com>
2026-04-19 20:41:38 -0400

    add smallest spotify image and millis to minutes and seconds utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,21 @@
 import type { ClassValue } from 'clsx';
 import { clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import type { SpotifyImage } from '#/types/spotify';
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export function getSmallestSpotifyImage(images: Array<SpotifyImage>) {
+	return images.length
+		? images.reduce((prev, curr) => (prev.height < curr.height ? prev : curr))
+		: null;
+}
+
+export const millisToMinutesAndSeconds = (millis: number) => {
+	const minutes = Math.floor(millis / 60000);
+	const seconds = ((millis % 60000) / 1000).toFixed(0);
+
+	return `${minutes}:${Number(seconds) < 10 ? '0' : ''}${seconds}`;
+};
