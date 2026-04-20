7b953c38ad72a0b682b582e1c93320fb29566eb1
Alex Knipfer <alexanderknipfer@gmail.com>
2026-04-19 20:41:46 -0400

    display top artists and tracks

--- src/routes/_protected.dashboard.tsx
+++ src/routes/_protected.dashboard.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { Music, TrendingUp } from 'lucide-react';
-import { Button } from '#/components/ui/button';
+import { ExternalLink, Music, TrendingUp } from 'lucide-react';
+import { Button, buttonVariants } from '#/components/ui/button';
 import {
 	Card,
 	CardAction,
@@ -8,7 +8,20 @@ import {
 	CardHeader,
 	CardTitle
 } from '#/components/ui/card';
+import {
+	Item,
+	ItemContent,
+	ItemDescription,
+	ItemGroup,
+	ItemMedia,
+	ItemTitle
+} from '#/components/ui/item';
 import { getTopStats } from '#/lib/spotify/spotify.functions';
+import {
+	cn,
+	getSmallestSpotifyImage,
+	millisToMinutesAndSeconds
+} from '#/lib/utils';
 
 export const Route = createFileRoute('/_protected/dashboard')({
 	loader: async () => getTopStats(),
@@ -17,10 +30,9 @@ export const Route = createFileRoute('/_protected/dashboard')({
 
 function RouteComponent() {
 	const data = Route.useLoaderData();
-	console.log('data: ', data);
 
 	return (
-		<div className="flex flex-row gap-2 px-3">
+		<div className="flex flex-row gap-6 px-10">
 			<Card className="w-full">
 				<CardHeader>
 					<CardAction>
@@ -37,7 +49,63 @@ function RouteComponent() {
 						Top Artists
 					</CardTitle>
 				</CardHeader>
-				<CardContent>Testing</CardContent>
+				<CardContent>
+					<ItemGroup>
+						{data.topArtists.items.map((artist) => {
+							const image = getSmallestSpotifyImage(artist.images);
+
+							return (
+								<Item
+									key={artist.id}
+									variant="outline"
+									role="listitem"
+									render={
+										<a href="/">
+											{image && (
+												<ItemMedia variant="image">
+													<img
+														src={image.url}
+														alt={artist.name}
+														width={32}
+														height={32}
+														className="object-cover rounded-lg"
+													/>
+												</ItemMedia>
+											)}
+											<ItemContent>
+												<ItemTitle className="line-clamp-1">
+													{artist.name}
+												</ItemTitle>
+												<ItemDescription className="line-clamp-1 text-xs">
+													<a
+														href={artist.external_urls.spotify}
+														target="_blank"
+														className={cn(
+															buttonVariants({
+																variant: 'ghost',
+																size: 'xs'
+															}),
+															'p-0 text-xs border-none'
+														)}
+													>
+														Open in spotify
+														<ExternalLink />
+													</a>
+												</ItemDescription>
+											</ItemContent>
+											<ItemContent className="flex-none text-center">
+												<ItemDescription className="text-xs">
+													{artist.followers.total.toLocaleString('en-US')}{' '}
+													followers
+												</ItemDescription>
+											</ItemContent>
+										</a>
+									}
+								/>
+							);
+						})}
+					</ItemGroup>
+				</CardContent>
 			</Card>
 			<Card className="w-full">
 				<CardHeader className="flex items-center justify-between">
@@ -53,6 +121,52 @@ function RouteComponent() {
 						See All
 					</Button>
 				</CardHeader>
+				<CardContent>
+					<ItemGroup>
+						{data.topTracks.items.map((track) => {
+							const artistNames = track.artists
+								.map((artist) => artist.name)
+								.join(', ');
+							const image = getSmallestSpotifyImage(track.album.images);
+
+							return (
+								<Item
+									key={track.id}
+									variant="outline"
+									role="listitem"
+									render={
+										<a href="/">
+											{image && (
+												<ItemMedia variant="image">
+													<img
+														src={image.url}
+														alt={`${track.name} album art`}
+														width={32}
+														height={32}
+														className="object-cover rounded-lg"
+													/>
+												</ItemMedia>
+											)}
+											<ItemContent className="pr-2.5">
+												<ItemTitle className="line-clamp-1">
+													{track.name}
+												</ItemTitle>
+												<ItemDescription className="line-clamp-1 text-xs">
+													{artistNames}&nbsp;·&nbsp;{track.album.name}
+												</ItemDescription>
+											</ItemContent>
+											<ItemContent>
+												<ItemDescription>
+													{millisToMinutesAndSeconds(track.duration_ms)}
+												</ItemDescription>
+											</ItemContent>
+										</a>
+									}
+								/>
+							);
+						})}
+					</ItemGroup>
+				</CardContent>
 			</Card>
 		</div>
 	);
